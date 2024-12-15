aa3155c56294d5d8c02d0899377d43f9ebc5008f
Hemna <waboring@hemna.com>
2024-12-14 19:09:16 -0500

    updated

--- direwolf_monitor/__init__.py
+++ direwolf_monitor/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import version, PackageNotFoundError
 __author__ = """Walter A. Boring IV"""
 __email__ = 'waboring@hemna.com'
 try:
-    __version__ = version("python_direwolf")
+    __version__ = version("direwolf_monitor")
 except PackageNotFoundError:
     # package is not installed
     pass
