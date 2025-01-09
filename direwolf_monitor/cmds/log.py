14fb732d4dabb5d80f08a5fe00afbf9f77120f03
Hemna <waboring@hemna.com>
2025-01-09 08:43:31 -0500

    some cleanup

--- direwolf_monitor/cmds/log.py
+++ direwolf_monitor/cmds/log.py
@@ -2,12 +2,9 @@ import logging
 from pathlib import Path
 import re
 import time
-from typing import Iterator, Optional
+from typing import Iterator
 
-import aprslib
 import click
-from haversine import Unit, haversine
-from aprsd.packets import core as aprsd_core
 import paho
 import paho.mqtt.client as mqtt
 from paho.mqtt.packettypes import PacketTypes
@@ -15,15 +12,10 @@ from paho.mqtt.properties import Properties
 
 from direwolf_monitor.cli import cli
 from direwolf_monitor import cli_helper
-from direwolf_monitor import utils
-from PIL import Image
-from term_image.image import AutoImage
+from direwolf_monitor.utils import packet as packet_utils
 
 
 LOG = logging.getLogger("dwm")
-
-symbol_chart0 = Image.open("aprs-symbols-128-0.png")
-symbol_chart1 = Image.open("aprs-symbols-128-1.png")
 
 
 def follow(file, sleep_sec=0.1) -> Iterator[str]:
@@ -90,152 +82,6 @@ def _create_mqtt_client(ctx, mqtt_host, mqtt_port, mqtt_username, mqtt_password,
         properties=properties
     )
     return client
-
-def _create_symbol_image(symbol, symbol_table):
-    symbol_dimension = 128
-    offset = ord(symbol) - 33
-    row = offset // 16
-    col = offset % 16
-    crop_area = (
-        col*symbol_dimension,
-        row*symbol_dimension,
-        col*symbol_dimension+symbol_dimension,
-        row*symbol_dimension+symbol_dimension
-    )
-    if symbol_table == "/":
-        test = symbol_chart0.crop(crop_area)
-    else:
-        test = symbol_chart1.crop(crop_area)
-        
-    real = AutoImage(test)
-    real.height=1
-    return real
-
-def _add_gps(logit, packet, my_latitude, my_longitude):
-    DISTANCE_COLOR = "#FF5733"
-    DEGREES_COLOR = "#FFA900"
-    if hasattr(packet, "latitude") and hasattr(packet, "longitude"):
-        my_coords = (float(my_latitude), float(my_longitude))
-        packet_coords = (packet.latitude, packet.longitude)
-        try:
-            bearing = utils.calculate_initial_compass_bearing(my_coords, packet_coords)
-        except Exception as e:
-            LOG.error(f"Failed to calculate bearing: {e}")
-            bearing = 0
-        logit.append(
-            f" : [{DEGREES_COLOR}]{utils.degrees_to_cardinal(bearing, full_string=True)}[/]"
-            f"[green]@[/][{DISTANCE_COLOR}]{haversine(my_coords, packet_coords, unit=Unit.MILES):.2f}[/] miles",
-        )
-
-
-def packet_print(ctx, packet: aprsd_core.Packet,
-                 latitude,
-                 longitude,
-                 tx: Optional[bool] = False,
-                 header: Optional[bool] = True) -> None:
-    FROM_COLOR = "#C70039"
-    TO_COLOR = "#D033FF"
-    
-    logit = []
-    name = packet.__class__.__name__
-
-    if header:
-        if tx:
-            via_color = "red"
-            arrow = f"[{via_color}]\u2192[/]"
-            logit.append(
-                f"[bold red]TX\u2191[/] "
-                f"[cyan]{name}[/]"
-                #f":{packet.msgNo}"
-                #f" ({packet.send_count + 1} of {pkt_max_send_count})",
-            )
-        else:
-            via_color = "#1AA730"
-            arrow = f"[{via_color}]\u2192[/]"
-            f"[{via_color}]<-[/]"
-            logit.append(
-                f"[#1AA730]RX\u2193[/] "
-                f"[cyan]{name}[/]"
-                #f":{packet.msgNo}",
-            )
-    else:
-        via_color = "green"
-        arrow = f"[{via_color}]->[/]"
-        logit.append(
-            f"[cyan]{name}[/]"
-            f":{packet.msgNo}",
-        )
-        
-    if hasattr(packet, "symbol"):
-        logit.append(" __XXIMAGEXX__ ")
-    else:
-        logit.append(" ")
-
-    tmp = None
-    if packet.path:
-        tmp = f"{arrow}".join(packet.path) + f"{arrow}"
-
-    logit.append(
-        f"[{FROM_COLOR}]{packet.from_call}[/]{arrow}"
-        f"{tmp if tmp else ' '}"
-        f"[{TO_COLOR}]{packet.to_call}[/]",
-    )
-    
-    if isinstance(packet, aprsd_core.ThirdPartyPacket):
-        # show the original tx -> rx callsigns
-        sub_pkt = packet.subpacket
-        if sub_pkt.path:
-            tmp = f"{arrow}".join(sub_pkt.path) + f"{arrow}"
-            
-        sub_head = f" ([{FROM_COLOR}]{sub_pkt.from_call}[/]{arrow}" \
-            f"{tmp if tmp else ' '}" \
-            f"[{TO_COLOR}]{sub_pkt.to_call}[/]) :"
-        
-        logit.append(
-            sub_head,
-        )
-        logit.append(
-            sub_pkt.human_info
-        )
-        
-        _add_gps(logit, sub_pkt, latitude, longitude)
-         
-    elif not isinstance(packet, aprsd_core.AckPacket) and not isinstance(packet, aprsd_core.RejectPacket):
-        logit.append(" : ")
-        msg = packet.human_info
-
-        if msg:
-            msg = msg.replace("<", "\\<")
-            logit.append(f"[bright_yellow]{msg}[/]")
-
-    # is there distance information?
-    #if isinstance(packet, aprsd_core.GPSPacket) and latitude and longitude:
-    _add_gps(logit, packet, latitude, longitude)
-    # if hasattr(packet, "latitude") and hasattr(packet, "longitude"):
-    #     my_coords = (float(latitude), float(longitude))
-    #     packet_coords = (packet.latitude, packet.longitude)
-    #     try:
-    #         bearing = utils.calculate_initial_compass_bearing(my_coords, packet_coords)
-    #     except Exception as e:
-    #         LOG.error(f"Failed to calculate bearing: {e}")
-    #         bearing = 0
-    #     logit.append(
-    #         f" : [{DEGREES_COLOR}]{utils.degrees_to_cardinal(bearing, full_string=True)}[/]"
-    #         f"[green]@[/][{DISTANCE_COLOR}]{haversine(my_coords, packet_coords, unit=Unit.MILES):.2f}[/] miles",
-    #     )
-
-    console = ctx.obj['console']
-    with console.capture() as capture:
-        for entry in logit:
-            console.print(entry, end="")
-    out_str = capture.get()
-    if "__XXIMAGEXX__" in out_str:
-        symbol_image = _create_symbol_image(packet.symbol, packet.symbol_table)
-        final_str = out_str.replace("__XXIMAGEXX__", "{symbol_image:1.1#}")
-        vars = {"symbol_image": symbol_image}
-        print(final_str.format(**vars))
-    else:
-        print(out_str)
 
 
 @cli.command()
@@ -400,17 +246,6 @@ def mqtt_to_terminal(ctx, mqtt_host, mqtt_port, mqtt_topic, mqtt_username, mqtt_
         console.print(f"Disconnected from mqtt server {mqtt_host} result code: {rc}")
         console.print(f"userdata: {userdata}")
         
-    def _parse_packet(raw):
-        try:
-            packet_json = aprslib.parse(raw)
-            return aprsd_core.factory(packet_json)
-        except aprslib.exceptions.ParseError:
-            # console.print(f"[bold red]Failed to parse '{raw}' because '{e}'")
-            pass
-        except aprslib.exceptions.UnknownFormat:
-            # console.print(f"[bold red]Failed to parse '{raw}' because '{e}'")
-            pass
-
     def _rx_on_message(client, userdata, msg):
         # console.out(f"{msg.topic} msg '{msg.payload}'")
         line = msg.payload.decode('UTF-8').strip()
@@ -421,19 +256,19 @@ def mqtt_to_terminal(ctx, mqtt_host, mqtt_port, mqtt_topic, mqtt_username, mqtt_
             packetstring = packetstring.replace('<0x0d>','\x0d'). \
                 replace('<0x1c>','\x1c').replace('<0x1e>','\x1e'). \
                 replace('<0x1f>','\0x1f').replace('<0x0a>','\0x0a')
-            packet = _parse_packet(packetstring)
+            packet = packet_utils.parse_packet(packetstring)
             if packet:
                 #aprsd_log.log(packet)
-                packet_print(ctx, packet, latitude=latitude, longitude=longitude)
+                packet_utils.packet_print(ctx, packet, latitude=latitude, longitude=longitude)
             #console.print(packet)
         elif "[0L]" in line:
             # packet that direwolf Transmitted
             raw = line.replace("[0L]", "").strip()
             #console.print(f"OL {raw}")
-            packet = _parse_packet(raw)
+            packet = packet_utils.parse_packet(raw)
             if packet:
                 #aprsd_log.log(packet, tx=True)
-                packet_print(ctx, packet, latitude=latitude, longitude=longitude, tx=True)
+                packet_utils.packet_print(ctx, packet, latitude=latitude, longitude=longitude, tx=True)
         # elif "[0H]" in line:
             # packet RX'd already covered?
             # raw = line.replace("[0H]", "").strip()
@@ -446,11 +281,11 @@ def mqtt_to_terminal(ctx, mqtt_host, mqtt_port, mqtt_topic, mqtt_username, mqtt_
             # Packet sent to direwolf from APRSIS
             # strip out the [ig]
             raw = line.replace("[ig]", "").strip()
-            #console.print(f"IG {raw}")
-            packet = _parse_packet(raw)
+            console.print(f"IG {raw}")
+            packet = packet_utils.parse_packet(raw)
             if packet:
                 #aprsd_log.log(packet)
-                packet_print(ctx, packet, latitude=latitude, longitude=longitude)
+                packet_utils.packet_print(ctx, packet, latitude=latitude, longitude=longitude)
             #console.print(packet)
         elif "[rx>ig]" in line:
             # Got a line from RF and sent to APRSIS 
@@ -462,11 +297,14 @@ def mqtt_to_terminal(ctx, mqtt_host, mqtt_port, mqtt_topic, mqtt_username, mqtt_
                 #console.out(f"Ignoring '{line}'")
         elif "[ig>tx]" in line:
             raw = line.replace("[ig>tx]", "").strip()
-            #console.print(f"IG>TX {raw}")
-            packet = _parse_packet(raw)
+            # console.print(f"IG>TX {raw}")
+            packet = packet_utils.parse_packet(raw)
             if packet:
                 # aprsd_log.log(packet)
-                packet_print(ctx, packet, latitude=latitude, longitude=longitude)
+                packet_utils.packet_print(
+                    ctx, packet, latitude=latitude, longitude=longitude,
+                    header="\[ig>tx]"
+                )
             
         elif 'ig_to_tx' in line:
             # ignoring
